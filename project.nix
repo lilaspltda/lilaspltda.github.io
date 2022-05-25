faaab81c89aad4ca5aa42d7d944ad8fd86291b27
hugosenari <hugosenari@gmail.com>
2022-05-25 19:18:37 -0300

    sveltekitu

---
+++ project.nix
@@ -0,0 +1,8 @@
+{ 
+  files.gitignore.enable = true;
+  files.gitignore.template."Global/Archives" = true;
+  files.gitignore.template."Global/Backup"   = true;
+  files.gitignore.template."Global/Diff"     = true;
+  files.gitignore.template."Node"     = true;
+  files.cmds.nodejs      = true; # lts
+}
