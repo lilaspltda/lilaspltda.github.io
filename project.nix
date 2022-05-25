8f7f81e7adab9b3991655150b66ccc1c7723015b
hugosenari <hugosenari@gmail.com>
2022-05-25 20:18:41 -0300

    chore: cloudflare

--- project.nix
+++ project.nix
@@ -3,6 +3,21 @@
   files.gitignore.template."Global/Archives" = true;
   files.gitignore.template."Global/Backup"   = true;
   files.gitignore.template."Global/Diff"     = true;
-  files.gitignore.template."Node"     = true;
+  files.gitignore.template."Node"            = true;
+  files.gitignore.pattern.".svelte-kit"      = true;
   files.cmds.nodejs      = true; # lts
+  files.alias.publish-as-cloudflare-from-local = ''
+    # same as publish-as-gh-pages but works local
+    cd $PRJ_ROOT
+    rm -rf .svelte-kit/cloudflare
+    ORIGIN=`git remote get-url origin`
+    npm run build
+    cd .svelte-kit/cloudflare
+    git init .
+    git add .
+    git checkout -b cloudflare
+    git commit -m "docs(cloudflare): update cloudflare pages" .
+    git remote add origin $ORIGIN
+    git push -u origin cloudflare --force
+  '';  
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
